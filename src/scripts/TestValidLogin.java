6049a332e0f8266a616b6e05a4697ea4f6a808bd
ajit <biswasajit1432@gmail.com>
2018-08-12 16:47:05 +0530

    updated the explicitwait from 20 sec to 30 sec

--- src/scripts/TestValidLogin.java
+++ src/scripts/TestValidLogin.java
@@ -30,7 +30,7 @@ public class TestValidLogin extends BaseTest{
 		//click on Login button
 		lp.clickLogin();
 		//Apply some condition -- using Explicit wait
-		WebDriverWait wait = new WebDriverWait(driver, 20);
+		WebDriverWait wait = new WebDriverWait(driver, 30);
 		wait.until(ExpectedConditions.titleIs("actiTIME - Enter Time-Track"));
 		SoftAssert sa = new SoftAssert();
 		sa.assertEquals(driver.getTitle(), "actiTIME - Enter Time-Track");
